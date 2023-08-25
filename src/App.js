88e85b262b00c5cd0b9f60f8c396ddeb85239743
Troy <tvinsixiengmay@gmail.com>
2023-08-24 22:15:25 -0700

    Rough country card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Where in the world?</h1>
+        </div>
       </header>
+      <body>
+        <CountryCard></CountryCard>
+      </body>
+    </div>
+  );
+}
+
+function CountryCard() {
+  return (
+    <div className="Country-card">
+      <img src="https://flagcdn.com/jp.svg" />
+      <h2>Japan</h2>
+      <p>
+        <strong>Population: </strong>125,836,021
+      </p>
+      <p>
+        <strong>Region: </strong>Asia
+      </p>
+      <p>
+        <strong>Capital: </strong>Tokyo
+      </p>
     </div>
   );
 }
